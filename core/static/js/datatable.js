346739576431e1779e574d8098a7c0681d7e5e2a
Phlopes777 <136033653+Phlopes777@users.noreply.github.com>
2023-06-30 17:50:59 -0300

    ajuste de relatorio

--- core/static/js/datatable.js
+++ core/static/js/datatable.js
@@ -1,12 +1,47 @@
 $(document).ready(function () {
-    $('#example').DataTable(
+    $('#tableVisitante').DataTable(
         {
             "language":{
                 "url": "//cdn.datatables.net/plug-ins/1.13.4/i18n/pt-BR.json",
-                order: [[0, 'desc']],
-            }
-            
-        }
+            },
+            order: [[0, 'desc']],
+            dom: 'Bfrtip',
+            buttons: [
+                'excelHtml5',
+                'pdfHtml5'
+            ]
+        },
+    )
+});
 
-    );
+$(document).ready(function () {
+    $('#tableControleVeiculo').DataTable(
+        {
+            "language":{
+                "url": "//cdn.datatables.net/plug-ins/1.13.4/i18n/pt-BR.json",
+            },
+            order: [[2, 'desc']],
+            dom: 'Bfrtip',
+            buttons: [
+                'excelHtml5',
+                'pdfHtml5'
+            ]
+        },
+    )
+});
+
+$(document).ready(function () {
+    $('#tableControleChave').DataTable(
+        {
+            "language":{
+                "url": "//cdn.datatables.net/plug-ins/1.13.4/i18n/pt-BR.json",
+            },
+            order: [[2, 'desc']],
+            dom: 'Bfrtip',
+            buttons: [
+                'excelHtml5',
+                'pdfHtml5'
+            ]
+        },
+    )
 });
